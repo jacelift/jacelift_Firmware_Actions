57629b2a853977a54a08ef9d36f1037550058f82
jacelift <74590290+jacelift@users.noreply.github.com>
2022-08-11 01:00:42 +0800

    Update mac80211.sh

--- files/lede17.01/TL-AP300C-POE-V1/mac80211.sh
+++ files/lede17.01/TL-AP300C-POE-V1/mac80211.sh
@@ -84,9 +84,9 @@ detect_mac80211() {
 		iw phy "$dev" info | grep -q 'Capabilities:' && htmode=HT20
 
 		iw phy "$dev" info | grep -q '5180 MHz' && {
-			mode_band="g"
-			channel="11"
-			iw phy "$dev" info | grep -q 'VHT Capabilities' && htmode="HT20"
+			mode_band="a"
+			channel="36"
+			iw phy "$dev" info | grep -q 'VHT Capabilities' && htmode="VHT80"
 		}
 
 		[ -n "$htmode" ] && ht_capab="set wireless.radio${devidx}.htmode=$htmode"
@@ -119,7 +119,7 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=AP300C-POE-V1
+			set wireless.default_radio${devidx}.ssid=OpenWrt
 			set wireless.default_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
