dd5b4aa639574ca358ddd8319ea00605766a81c7
jacelift <74590290+jacelift@users.noreply.github.com>
2023-01-06 20:26:33 +0800

    Update kiddin9_diy-part2.sh

--- kiddin9_diy-part2.sh
+++ kiddin9_diy-part2.sh
@@ -7,7 +7,7 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Description: OpenWrt DIY script part 2 (After Update and Install feeds)
 #
 scripts/feeds install -a -p kiddin9 -f
 scripts/feeds install -a
